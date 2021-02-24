64e8732a7889fe8bed953ccb2bf3eb9403ffcef3
NikolenkoMaksim <kinik33@mail.ru>
2021-02-24 18:32:51 +0300

    Исправлены замечания

--- working_with_arrays/workingWithArrayScript.js
+++ working_with_arrays/workingWithArrayScript.js
@@ -1,44 +1,61 @@
-var ETR = {};
-ETR.array1 = [15, 2, 0, 5, -3, 16, 4, 18, 9, 5];
+var array1 = [15, 2, 0, 5, -3, 16, 4, 18, 9, 5];
 
-(function (array) {
-    array.sort(function (e1, e2) {
+function descendingSort(array) {
+    return array.sort(function (e1, e2) {
         return e2 - e1;
-    })
-})(ETR.array1);
+    });
+}
 
-console.log(ETR.array1);
+array1 = descendingSort(array1);
 
-ETR.array2 = (function (array, elementsAmount) {
-    return array.slice(0, elementsAmount)
-})(ETR.array1, 5);
-console.log(ETR.array2);
+console.log(array1);
 
-ETR.array3 = (function (array, elementsAmount) {
-    return array.slice(array.length - elementsAmount)
-})(ETR.array1, 5)
-console.log(ETR.array3);
+function getFirstElements(array, elementsAmount) {
+    return array.slice(0, elementsAmount);
+}
 
-console.log(ETR.array1.reduce(function (accumulator, currentElement) {
-    if (currentElement % 2 === 0) {
-        return accumulator + currentElement;
-    } else {
-        return accumulator;
-    }
-}, 0));
+var array2 = getFirstElements(array1, 5);
+console.log(array2);
 
-ETR.array4 = [];
-(function (array, startNumber, finishNumber) {
+function getLastElements(array, elementsAmount) {
+    return array.slice(array.length - elementsAmount);
+}
+
+var array3 = getLastElements(array1, 5);
+console.log(array3);
+
+function summariseEvenNumbers(array) {
+    return array.reduce(function (accumulator, currentElement) {
+        if (currentElement % 2 === 0) {
+            return accumulator + currentElement;
+        } else {
+            return accumulator;
+        }
+    }, 0);
+}
+
+console.log(summariseEvenNumbers(array1));
+
+var array4 = [];
+
+function fillArrayByNumbers(array, startNumber, finishNumber) {
     for (var i = startNumber; i <= finishNumber; i++) {
         array.push(i);
     }
-})(ETR.array4, 1, 100);
-console.log(ETR.array4);
 
-console.log(ETR.array4.reduce(function (accumulator, currentElement) {
-    if (currentElement % 2 === 0) {
-        return accumulator + Math.pow(currentElement, 2);
-    } else {
-        return accumulator;
-    }
-}, 0));
+    return array;
+}
+
+console.log(fillArrayByNumbers(array4, 1, 100));
+
+function summariseEvenNumberSquares(array) {
+    return array.reduce(function (accumulator, currentElement) {
+        if (currentElement % 2 === 0) {
+            return accumulator + Math.pow(currentElement, 2);
+        } else {
+            return accumulator;
+        }
+    }, 0);
+}
+
+console.log(summariseEvenNumberSquares(array4));
