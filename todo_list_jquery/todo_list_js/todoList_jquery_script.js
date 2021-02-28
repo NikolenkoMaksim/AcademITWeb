5afb618a97af586274a190c41c3cb553d767c5ab
NikolenkoMaksim <kinik33@mail.ru>
2021-02-28 19:38:35 +0300

    Внесены изменения в задачу Todo List jQuery, учитывающие замечания по другим задачам

--- todo_list_jquery/todo_list_js/todoList_jquery_script.js
+++ todo_list_jquery/todo_list_js/todoList_jquery_script.js
@@ -8,12 +8,16 @@ $(document).ready(function () {
         var noteText = inputTextField.val();
 
         if (noteText === "") {
-            errorMessageBlock.text("Пожалуйста, заполните поле ввода");
+            errorMessageBlock.removeClass("empty_block").text("Пожалуйста, заполните поле ввода");
             return;
         }
 
+        errorMessageBlock.addClass("empty_block");
+
         function setViewMode(listItem) {
-            listItem.html("<span class='note_text'></span><button class='edit_button' type='button'>Редактировать</button><button class='delete_button' type='button'>Удалить</button>");
+            listItem.html("<span class='note_text'></span>" +
+                "<button class='edit_button' type='button'>Редактировать</button>" +
+                "<button class='delete_button' type='button'>Удалить</button>");
 
             listItem.find(".note_text").text(noteText);
 
@@ -22,13 +26,18 @@ $(document).ready(function () {
             });
 
             listItem.find(".edit_button").click(function () {
-                listItem.html("<div><input class='edit_text' type='text' maxlength='58'><button class='save_button' type='button'>Сохранить</button><button class='cancel_button' type='button'>Закрыть</button></div><div class='error_message_block'></div>");
+                listItem.html("<div>" +
+                    "<input class='edit_text' type='text' maxlength='58'>" +
+                    "<button class='save_button' type='button'>Сохранить</button>" +
+                    "<button class='cancel_button' type='button'>Закрыть</button>" +
+                    "</div>" +
+                    "<div class='error_message_block hidden_block'></div>");
 
                 listItem.find(".edit_text").val(noteText);
 
                 listItem.find(".save_button").click(function () {
                     if (listItem.find(".edit_text").val() === "") {
-                        listItem.find(".error_message_block").text("Пожалуйста, заполните поле ввода");
+                        listItem.find(".error_message_block").removeClass("hidden_block").text("Пожалуйста, заполните поле ввода");
                         return;
                     }
 
