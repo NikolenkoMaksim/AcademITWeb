71c13fc75e0a00473ef147823ea378efa2ab3e42
NikolenkoMaksim <kinik33@mail.ru>
2021-02-28 17:59:51 +0300

    Исправлены замечания по задаче Temperature Converter

--- temperature_converter/temperatureConverterScript.js
+++ temperature_converter/temperatureConverterScript.js
@@ -2,26 +2,28 @@ document.addEventListener("DOMContentLoaded", function () {
     var inputTextField = document.getElementById("input_text_field");
     var outputTextBlock = document.querySelector(".output_text_block");
 
-
     document.getElementById("convert_button").addEventListener("click", function () {
         var inputTextValue = inputTextField.value;
 
         if (inputTextValue === "") {
             outputTextBlock.textContent = "Пожалуйста, заполните поле ввода";
+            outputTextBlock.classList.add("error_message");
             return;
         }
 
         if (isNaN(inputTextValue)) {
-            outputTextBlock.textContent = "В поле ввода присутсвуют недопустимые символы";
+            outputTextBlock.textContent = "В поле ввода присутствуют недопустимые символы";
+            outputTextBlock.classList.add("error_message");
             return;
         }
 
         inputTextField.value = "";
+        outputTextBlock.classList.remove("error_message");
 
-        var valueInKelvin = Number.parseFloat(inputTextValue) + 273.15;
-        var valueInFahrenheit = inputTextValue * 1.8 + 32;
+        var temperatureInKelvin = parseFloat(inputTextValue) + 273.15;
+        var temperatureInFahrenheit = inputTextValue * 1.8 + 32;
 
-        outputTextBlock.textContent = inputTextValue + "° Цельсия = " + Math.round(valueInKelvin * 1000) / 1000 +
-            "° Кельвина = " + Math.round(valueInFahrenheit * 1000) / 1000 + "° Фаренгейта";
+        outputTextBlock.textContent = inputTextValue + "° Цельсия = " + parseFloat(temperatureInKelvin.toFixed(3)) +
+            "° Кельвина = " + parseFloat(temperatureInFahrenheit.toFixed(3)) + "° Фаренгейта";
     })
 });
