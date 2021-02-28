9eb2e0c3e03f3ebaed029ac48ed614fc9bf5f679
NikolenkoMaksim <kinik33@mail.ru>
2021-02-28 19:25:03 +0300

    Внесены изменения с учетом замечаний по другим задачам

--- todo_list/todoListScript.js
+++ todo_list/todoListScript.js
@@ -9,11 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (noteText === "") {
             errorMessageBlock.textContent = "Пожалуйста, заполните поле ввода";
+            errorMessageBlock.classList.remove("empty_block");
             return;
         }
 
+        errorMessageBlock.classList.add("empty_block");
+
         function setViewMode(listItem) {
-            listItem.innerHTML = "<span class='note_text'></span><button class='edit_button' type='button'>Редактировать</button><button class='delete_button' type='button'>Удалить</button>";
+            listItem.innerHTML = "<span class='note_text'></span>" +
+                "<button class='edit_button' type='button'>Редактировать</button>" +
+                "<button class='delete_button' type='button'>Удалить</button>";
 
             listItem.querySelector(".note_text").textContent = noteText;
 
@@ -22,19 +27,25 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             listItem.querySelector(".edit_button").addEventListener("click", function () {
-                listItem.innerHTML = "<div><input class='edit_text' type='text'><button class='save_button' type='button'>Сохранить</button><button class='cancel_button' type='button'>Закрыть</button></div><div class='error_message_block'></div>";
+                listItem.innerHTML = "<div>" +
+                    "<input class='edit_text' type='text'>" +
+                    "<button class='save_button' type='button'>Сохранить</button>" +
+                    "<button class='cancel_button' type='button'>Закрыть</button>" +
+                    "</div>" +
+                    "<div class='error_message_block hidden_block'></div>";
 
                 listItem.querySelector(".edit_text").value = noteText;
                 listItem.querySelector(".edit_text").maxLength = 58;
 
-
                 listItem.querySelector(".save_button").addEventListener("click", function () {
                     if (listItem.querySelector(".edit_text").value === "") {
                         listItem.querySelector(".error_message_block").textContent = "Пожалуйста, заполните поле ввода";
+                        listItem.querySelector(".error_message_block").classList.remove("hidden_block");
                         return;
                     }
 
                     listItem.querySelector(".error_message_block").textContent = "";
+                    listItem.querySelector(".error_message_block").classList.add("empty_block")
                     noteText = listItem.querySelector(".edit_text").value;
                     setViewMode(listItem);
                 });
