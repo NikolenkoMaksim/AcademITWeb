dfeb9528d9fd320db70c9936c93579da57732ee7
NikolenkoMaksim <kinik33@mail.ru>
2021-03-09 10:42:08 +0700

    Исправлены замечания по задаче "Работа с массивами"

--- working_with_arrays/workingWithArrayScript.js
+++ working_with_arrays/workingWithArrayScript.js
@@ -1,61 +1,59 @@
-var array1 = [15, 2, 0, 5, -3, 16, 4, 18, 9, 5];
+(function () {
+    var array1 = [15, 2, 0, 5, -3, 16, 4, 18, 9, 5];
 
-function descendingSort(array) {
-    return array.sort(function (e1, e2) {
-        return e2 - e1;
-    });
-}
-
-array1 = descendingSort(array1);
-
-console.log(array1);
-
-function getFirstElements(array, elementsAmount) {
-    return array.slice(0, elementsAmount);
-}
-
-var array2 = getFirstElements(array1, 5);
-console.log(array2);
-
-function getLastElements(array, elementsAmount) {
-    return array.slice(array.length - elementsAmount);
-}
-
-var array3 = getLastElements(array1, 5);
-console.log(array3);
-
-function summariseEvenNumbers(array) {
-    return array.reduce(function (accumulator, currentElement) {
-        if (currentElement % 2 === 0) {
-            return accumulator + currentElement;
-        } else {
-            return accumulator;
-        }
-    }, 0);
-}
-
-console.log(summariseEvenNumbers(array1));
-
-var array4 = [];
-
-function fillArrayByNumbers(array, startNumber, finishNumber) {
-    for (var i = startNumber; i <= finishNumber; i++) {
-        array.push(i);
+    function sortDescending(array) {
+        return array.sort(function (e1, e2) {
+            return e2 - e1;
+        });
     }
 
-    return array;
-}
+    console.log(sortDescending(array1));
 
-console.log(fillArrayByNumbers(array4, 1, 100));
+    function getFirstElements(array, elementsAmount) {
+        return array.slice(0, elementsAmount);
+    }
 
-function summariseEvenNumberSquares(array) {
-    return array.reduce(function (accumulator, currentElement) {
-        if (currentElement % 2 === 0) {
-            return accumulator + Math.pow(currentElement, 2);
-        } else {
+    var array2 = getFirstElements(array1, 5);
+    console.log(array2);
+
+    function getLastElements(array, elementsAmount) {
+        return array.slice(array.length - elementsAmount);
+    }
+
+    var array3 = getLastElements(array1, 5);
+    console.log(array3);
+
+    function getEvenNumbersSum(array) {
+        return array.reduce(function (accumulator, currentElement) {
+            if (currentElement % 2 === 0) {
+                return accumulator + currentElement;
+            }
+
             return accumulator;
-        }
-    }, 0);
-}
+        }, 0);
+    }
 
-console.log(summariseEvenNumberSquares(array4));
+    console.log(getEvenNumbersSum(array1));
+
+    var array4 = [];
+
+    function fillArrayWithNumbers(array, startNumber, finishNumber) {
+        for (var i = startNumber; i <= finishNumber; i++) {
+            array.push(i);
+        }
+
+        return array;
+    }
+
+    console.log(fillArrayWithNumbers(array4, 1, 100));
+
+    function getEvenNumbersSquares(array) {
+        return array.filter(function selectEvenNumbers(number) {
+            return number % 2 === 0;
+        }).map(function squareNumbers(currentValue) {
+            return currentValue * currentValue;
+        });
+    }
+
+    console.log(getEvenNumbersSquares(array4));
+})();
