bcba794eb613fd919a82dc2dbda2ebab80ed4a64
NikolenkoMaksim <kinik33@mail.ru>
2021-04-01 22:15:24 +0700

    Исправлены замечания по задаче "Phone book Vue"

--- phone_book_vue/phone_book_vue_script.js
+++ phone_book_vue/phone_book_vue_script.js
@@ -2,7 +2,7 @@ new Vue({
     el: "#app",
 
     data: {
-        contacts_data_inputs: [
+        contactsDataInputs: [
             {
                 id: "lastNameInput",
                 name: "Фамилия",
@@ -34,42 +34,42 @@ new Vue({
         addContact: function () {
             var isValidationFailed = false;
 
-            this.contacts_data_inputs.forEach(function (contact_data_input) {
-                if (contact_data_input.text.trim() === "") {
+            this.contactsDataInputs.forEach(function (contactDataInput) {
+                if (contactDataInput.text.trim() === "") {
                     isValidationFailed = true;
-                    contact_data_input.isInvalid = true;
-                    contact_data_input.text = "";
+                    contactDataInput.isInvalid = true;
+                    contactDataInput.text = "";
                 } else {
-                    contact_data_input.isInvalid = false;
+                    contactDataInput.isInvalid = false;
                 }
             });
 
             if (isValidationFailed) {
-                this.contacts_data_inputs[2].errorMessage = "Пожалуйста, заполните поле";
+                this.contactsDataInputs[2].errorMessage = "Пожалуйста, заполните поле";
                 return;
             }
 
             var self = this;
 
             if (this.contacts.some(function (c) {
-                return c.phoneNumber === self.contacts_data_inputs[2].text;
+                return c.phoneNumber === self.contactsDataInputs[2].text;
             })) {
-                self.contacts_data_inputs[2].isInvalid = true;
-                self.contacts_data_inputs[2].errorMessage = "Контакт с таким номером уже существует";
+                self.contactsDataInputs[2].isInvalid = true;
+                self.contactsDataInputs[2].errorMessage = "Контакт с таким номером уже существует";
                 return;
             }
 
             this.contacts.push({
                 id: this.contactId,
-                lastName: this.contacts_data_inputs[0].text,
-                firstName: this.contacts_data_inputs[1].text,
-                phoneNumber: this.contacts_data_inputs[2].text
+                lastName: this.contactsDataInputs[0].text,
+                firstName: this.contactsDataInputs[1].text,
+                phoneNumber: this.contactsDataInputs[2].text
             });
 
             this.contactId++;
 
-            this.contacts_data_inputs.forEach(function (contact_data_input) {
-                contact_data_input.text = "";
+            this.contactsDataInputs.forEach(function (contactDataInput) {
+                contactDataInput.text = "";
             });
         },
 
